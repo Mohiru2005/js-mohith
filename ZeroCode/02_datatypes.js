6589d5e3450b226ad7fc104a6a1932cf450ae7eb
mohith D <163638586+Mohiru2005@users.noreply.github.com>
2026-09-02 22:42:53 +0530

    data types

---
+++ ZeroCode/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";  
+// Treat all JS code as newer  version of JavaScript
+//Usually semi columns are not mandatory in JavaScript we use ";" for better readability 
+
+let name = 'mohith'
+let age = 21
+let isLoggedIn = true
+
+// number range is around 2 power 53
+// bigint
+// string is either '' or ""
+// boolean is true/false
+// null is standalone value 
+// undefined is like the value is not assigned
+// symbol is like when we talk about unique things
+/* These are primitive data types */
+
+//object
+
+console.log(typeof null);       //It is a object
+console.log(typeof undefined);  //undefined
+
