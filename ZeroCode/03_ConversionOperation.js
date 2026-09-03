49b6587897b182a79b4ebbcead5b626ba3779da6
mohith D <163638586+Mohiru2005@users.noreply.github.com>
2026-09-03 09:08:24 +0000

    Operations

--- ZeroCode/03_ConversionOperation.js
+++ ZeroCode/03_ConversionOperation.js
@@ -5,12 +5,12 @@
 //for example
 //console.log(typeof score); // When we run this its a number
 let score = "33"
-console.log(typeof score);
+//console.log(typeof score);
 // Now its a string so we have to convert it into the data type which we want
 
 // Now we will convert the String into a integer
 let rank = Number(score)
-console.log(rank);
+//console.log(rank);
 // Now it is converted into Number and stored in the memory address of rank
 // What if the string is combination of numbers and words will js still be able to convert?
 // The Anwser is YES because it does convert the combo but it returns a attribute called NaN (Not a Number)
@@ -18,3 +18,40 @@ console.log(rank);
 // If the value is undefined it will also return NaN
 // If the value is a boolean if its true it will return 1 or if its false it will return 0
 // If the value is a pure String it will also return Not a Number
+
+//Also there is something special about boolean let us see with an example 
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//If we use a empty string we get the value false if we write something in in that string it takes as true
+
+
+//------------------------------------------- Operations ---------------------------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
