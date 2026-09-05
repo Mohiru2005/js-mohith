4ac9e72acc94f375fa0fde7cf931c9bcc1f2f422
mohith D <163638586+Mohiru2005@users.noreply.github.com>
2026-09-05 07:55:52 +0000

    data types more

--- ZeroCode/02_datatypes.js
+++ ZeroCode/02_datatypes.js
@@ -19,4 +19,40 @@ let isLoggedIn = true
 
 console.log(typeof null);       //It is a object
 console.log(typeof undefined);  //undefined
+//#--------------------------------------------------------------------------------------------------------------------------------#
 
+//Primitive data type(call by value is where orginal data isnt given we will get a copy)
+
+// 7 types - String , Number , Boolean , null , undefined , Symbol , BigInt
+
+
+
+
+//const id = Symbol("123")
+//const anotherId = Symbol("123")
+
+//console.log(id===anotherId);  //Symbol represents a unique value
+
+
+//Non Primitive data type (call by reference orginal data will be accessed)
+// Array , Objects , Functions 
+// Js is dynamically typed
+
+// Arrays 
+
+const heroes = ["goku" , "luffy" , "naruto chapri" , "ichigoat"]
+
+//We have objects under curly braces they are in key pair values
+
+let myObj=
+{
+    name : "mohith",
+    age : 21,
+}
+
+//Fuctions also treated as a variable in js
+const myFucntion=myfunc()
+{
+    console.log("hello world");
+    
+}
